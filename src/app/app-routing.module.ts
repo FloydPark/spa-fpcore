e104df64854cf4d3817387c24618b23713668eba
nancho313 <nancho313@hotmail.com>
2020-06-03 11:06:58 -0500

    add route path to catalog/product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'catalog/product',
+    component: ProductComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
